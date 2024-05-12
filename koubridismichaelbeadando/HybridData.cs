2f32e198d6e109359328439e8c784d769afb98fa
kmike11 <k.michael1154@gmail.com>
2024-05-12 17:00:49 +0200

    final_code_upload

---
+++ koubridismichaelbeadando/HybridData.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace koubridismichaelbeadando
+{
+    public class HybridData
+    {
+        private HybridDictionary data;
+
+        public HybridData()
+        {
+            data = new HybridDictionary();
+        }
+
+        public void Add(string key, object value)
+        {
+            data.Add(key, value);
+        }
+
+        public void Remove(string key)
+        {
+            if (data.Contains(key))
+            {
+                data.Remove(key);
+            }
+        }
+
+        public object Get(string key)
+        {
+            if (data[key] is not null)
+            {
+                return data[key];
+            }
+            else
+            {
+                throw new Exception($"A {key} kulcsnak nincs értéke.");
+            }
+        }
+
+        public void Clear()
+        {
+            data.Clear();
+        }
+
+        public int Count()
+        {
+            return data.Count;
+        }
+
+        public bool Contains(string key)
+        {
+            return data.Contains(key);
+        }
+
+        public IEnumerable GetAllKeys()
+        {
+            foreach (var key in data.Keys)
+            {
+                yield return key;
+            }
+        }
+
+        public IEnumerable GetKeysUntil(string stopKey)
+        {
+            foreach (var key in data.Keys)
+            {
+                if (key.ToString() == stopKey)
+                    yield break;
+                yield return key;
+            }
+        }
+    }
+}
---
+++ koubridismichaelbeadando/Program.cs
@@ -0,0 +1,54 @@
+﻿namespace koubridismichaelbeadando
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            HybridData hybridData = new HybridData();
+
+            for (int i = 0; i < 20; i++)
+            {
+                hybridData.Add($"Key{i}", $"Value{i}");
+            }
+
+            Console.WriteLine("Néhány érték kulcshivatkozással:");
+            Console.WriteLine(hybridData.Get("Key1"));
+            Console.WriteLine(hybridData.Get("Key19"));
+
+            Console.WriteLine("A Key5-t létezik-e: " + hybridData.Contains("Key5"));
+
+            hybridData.Remove("Key5");
+            Console.WriteLine("Key5 törölve");
+            hybridData.Remove("Key10");
+
+            try
+            {
+                Console.WriteLine(hybridData.Get("Key5"));
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            Console.WriteLine($"Törlés utáni elemszám: {hybridData.Count()}");
+
+            Console.WriteLine("Össz. kulcs listázása:");
+            foreach (var key in hybridData.GetAllKeys())
+            {
+                Console.WriteLine(key);
+            }
+
+            Console.WriteLine("Key3-ig listázott kulcsok:");
+            foreach (var key in hybridData.GetKeysUntil("Key3"))
+            {
+                Console.WriteLine(key);
+            }
+
+            hybridData.Clear();
+
+            Console.WriteLine($"Elemszám törlés után: {hybridData.Count()}");
+
+            Console.ReadLine();
+        }
+    }
+}
